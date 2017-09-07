131807c431f2ed8dbfd4456a41c1c34312525101
Yann Pouillon <devops@materialsevolution.es>
2017-09-07 17:31:42 +0200

    Fixed copyright notices

--- examples/system/write_system_metadata.F90
+++ examples/system/write_system_metadata.F90
@@ -1,22 +1,21 @@
-!! Copyright (C) 2015-2016 Micael Oliveira <micael.oliveira@mpsd.mpg.de>
-!!                         Yann Pouillon <notifications@materialsevolution.es>
+!! Copyright (C) 2017 Yann Pouillon <devops@materialsevolution.es>
 !!
-!! This file is part of Libpspio.
+!! This file is part of ESCDF-Fortran.
 !!
-!! Libpspio is free software: you can redistribute it and/or modify it under
-!! the terms of the GNU Lesser General Public License as published by the Free
-!! Software Foundation, version 3 of the License, or (at your option) any later
-!! version.
+!! ESCDF-Fortran is free software: you can redistribute it and/or modify it
+!! under the terms of the GNU Lesser General Public License as published by the
+!! Free Software Foundation, version 2.1 of the License, or (at your option)
+!! any later version.
 !!
-!! Libpspio is distributed in the hope that it will be useful, but WITHOUT ANY
-!! WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-!! FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
-!! more details.
+!! ESCDF-Fortran is distributed in the hope that it will be useful, but WITHOUT
+!! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!! FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+!! for more details.
 !!
 !! You should have received a copy of the GNU Lesser General Public License
-!! along with Libpspio.  If not, see <http://www.gnu.org/licenses/> or write to
-!! the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-!! 02110-1301  USA.
+!! along with ESCDF-Fortran.  If not, see <http://www.gnu.org/licenses/> or
+!! write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+!! Boston, MA  02110-1301  USA.
 
 program write_system_metadata
 
